f61de3fe096bc8eb9bf89782b28fb20a73bae3d6
CamiNunes <cvn.camila@gmail.com>
2020-01-25 17:06:14 -0300

    variaveis e seletores com javascript;

---
+++ index.js
@@ -0,0 +1,9 @@
+var nome = document.querySelector("#exampleInputName");
+var genero = document.querySelectorAll("#form-user-create [name=gender]:checked");
+var nascimento = document.querySelector("#exampleInputBirth");
+var pais = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var senha = document.querySelector("#exampleInputPassword");
+var foto = document.querySelector("#exempleInputFile");
+var admin = document.querySelector("#admin");
+
