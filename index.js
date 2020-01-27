8c35e6e50f6b61894be3cf19cf74dca95308fa3e
CamiNunes <cvn.camila@gmail.com>
2020-01-26 21:59:39 -0300

    Trabalhandocom ForEach, if e else;

--- index.js
+++ index.js
@@ -5,5 +5,21 @@ var pais = document.querySelector("#exampleInputCountry");
 var email = document.querySelector("#exampleInputEmail");
 var senha = document.querySelector("#exampleInputPassword");
 var foto = document.querySelector("#exempleInputFile");
-var admin = document.querySelector("#admin");
+var admin = document.querySelector("#exempleInputAdmin");
+
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach(function(field, index){
+    if (field.name == "gender"){
+        if (field.checked){
+            console.log("SIM", field);
+        }
+    } else {
+        console.log("NÃO", field);
+    }
+
+
+    //console.log(field.name);
+});
 
